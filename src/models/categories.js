5fed6cedf083a4bc97f167bb9326f0a33b6648be
JoshuaJP2002 <jjimenezperalta@byupathway.edu>
2026-09-17 23:51:58 -0400

    update

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,6 +1,6 @@
 import db from './db.js'
 
-const getAllCategories = async() => {
+const getAllCategories = async () => {
     const query = `
         SELECT category_id, category_name
         FROM categories
@@ -8,8 +8,65 @@ const getAllCategories = async() => {
     `;
 
     const result = await db.query(query);
+    return result.rows;
+};
+
+const getCategoryDetails = async (id) => {
+    const query = `
+        SELECT category_id, category_name
+        FROM categories
+        WHERE category_id = $1;
+    `;
+
+    const queryParams = [id];
+    const result = await db.query(query, queryParams);
+
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+const getCategoriesByProjectId = async (projectId) => {
+    const query = `
+        SELECT
+            c.category_id,
+            c.category_name
+        FROM categories c
+        JOIN project_category pc
+            ON c.category_id = pc.category_id
+        WHERE pc.project_id = $1
+        ORDER BY c.category_name ASC;
+    `;
+
+    const queryParams = [projectId];
+    const result = await db.query(query, queryParams);
 
     return result.rows;
-}
+};
 
-export {getAllCategories}
+const getProjectsByCategoryId = async (categoryId) => {
+    const query = `
+        SELECT
+            p.project_id,
+            p.title,
+            p.description,
+            p.location,
+            p.project_date AS date,
+            p.organization_id
+        FROM projects p
+        JOIN project_category pc
+            ON p.project_id = pc.project_id
+        WHERE pc.category_id = $1
+        ORDER BY p.project_date ASC;
+    `;
+
+    const queryParams = [categoryId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+export {
+    getAllCategories,
+    getCategoryDetails,
+    getCategoriesByProjectId,
+    getProjectsByCategoryId
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { showHomePage } from "./controllers/index.js";
-import { showCategoriesPage } from "./controllers/categories.js";
+import { showCategoriesPage, showCategoryDetailsPage } from "./controllers/categories.js";
 import { showProjectsPage, showProjectDetailsPage } from "./controllers/projects.js";
 import { showOrganizationsPage, showOrganizationDetailsPage } from "./controllers/organizations.js";
 import { testErrorPage } from "./controllers/errors.js";
@@ -10,6 +10,7 @@ const router = express.Router();
 
 router.get("/", showHomePage);
 router.get("/categories", showCategoriesPage);
+router.get("/category/:id", showCategoryDetailsPage);
 router.get("/projects", showProjectsPage);
 router.get("/project/:id", showProjectDetailsPage);
 router.get("/organizations", showOrganizationsPage);
